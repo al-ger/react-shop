b290e52e1737ab5f2c62ed85dc3618bc78c43138
al-ger <geru41989@gmail.com>
2021-04-04 13:46:55 +0100

    gh-pages added

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,12 +2,18 @@ function Header() {
     return (
         <nav className="#43a047 green darken-1">
             <div className="nav-wrapper">
-                <a href="#" className="brand-logo">
+                <a href="/" className="brand-logo">
                     React Shop
                 </a>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
                     <li>
-                        <a href="!#">Repo</a>
+                        <a
+                            href="https://github.com/al-ger/react-shop"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            Repo
+                        </a>
                     </li>
                 </ul>
             </div>
